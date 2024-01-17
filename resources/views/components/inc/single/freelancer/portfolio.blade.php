a6c9ba7d2ce180e5e5967b6b81956f0e2beda7c0
artemdiduk <artem_didyk@ukr.net>
2024-01-17 17:01:34 +0200

    fixed picture portfolio

--- resources/views/components/inc/single/freelancer/portfolio.blade.php
+++ resources/views/components/inc/single/freelancer/portfolio.blade.php
@@ -9,7 +9,7 @@
                 @if (!empty($portfolio->image))
                     <a href="{{ asset('storage/portfolio/' . $portfolio->image) }}"
                         class="single-freelancer-portfolio-elm" data-fancybox="portfolio">
-                        <img class="single-freelancer-portfolio-elm" alt="{{ $portfolio->title }}"
+                        <img class="single-freelancer-portfolio-elm-bg" alt="{{ $portfolio->title }}"
                             src="{{ asset('storage/portfolio/' . $portfolio->image) }}">
                         <div class="single-freelancer-portfolio-elm-blue"></div>
                         <img class="single-freelancer-portfolio-elm-ico" src="/images/icons/zoom.svg" alt="zoom">
